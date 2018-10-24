28b88fc0b5532792f7af857f0bbebbd7a9b6dd2c
Muhammad Faraz <muhammadfaraz840@gmail.com>
2018-10-24 22:51:07 +0500

    Faculty page and event page is created and relivent data is added

---
+++ Student Management System/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Management_System.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EventDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ Student Management System/Controllers/FacultyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Management_System.Controllers
+{
+    public class FacultyController : Controller
+    {
+        // GET: Faculty
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TeacherDetails()
+        {
+            return View();
+        }
+    }
+}
